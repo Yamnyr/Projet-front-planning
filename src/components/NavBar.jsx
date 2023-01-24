b8c8aa3dcc0d896aa7bbf0c1acf082f673253022
XyloxExe <maximec02@live.fr>
2023-01-24 14:41:42 +0100

    navbar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,25 +4,25 @@ import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
   return (
-    <nav style={{ backgroundColor: "#42BDF0", height: 100, alignItems: "center" }}>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <img src="logo.png" alt="logo" />
-        <div style={{ display: "flex", alignItems: "center"}}>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src="logo.png" alt="logo" className="logo"/>
+        <div className="navbar-text">
           <FontAwesomeIcon icon={faUsers} style={{ marginRight: "5px" }} />
-          <h1 style={{ color: "white", fontSize: 25 }}>Les groupes</h1>
+          <div className="group-title">
+            <a href="#">Les groupes</a>
+          </div>
         </div>
-        <h1 style={{ color: "white", fontSize: 40, flex: 1, textAlign: 'center' }}>Planning événementiel</h1>
-        <div style={{ display: "flex", alignItems: "center", marginRight: 20 }}>
+      </div>
+      <div className="group-title">
+        <p className="navbar-text">Planning événementiel</p>
+      </div>
+      <div className="navbar-right">
+        <div className="navbar-text">
           <FontAwesomeIcon icon={faUser} style={{ marginRight: "5px" }} />
-          <h1 style={{ color: "white", fontSize: 25, marginLeft: "10px" }}>
-            Bonjour User
-          </h1>
+          <div className="group-title">
+            <a href="#">Bonjour User</a>
+          </div>
         </div>
       </div>
     </nav>
