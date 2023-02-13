29c267215241d231463e4a24b1dc8a18b8d6d962
XyloxExe <maximec02@live.fr>
2023-02-13 11:16:31 +0100

    Modification de la navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,47 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faUsers,
+  faCalendarDay,
+} from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  }, []);
+
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <img src="logo.png" alt="logo" className="logo"/>
-        <div className="navbar-text">
-          <FontAwesomeIcon icon={faUsers} style={{ marginRight: "5px" }} />
-          <div className="group-title">
-            <a href="#">Les groupes</a>
-          </div>
-        </div>
-      </div>
+    <nav className={`navbar ${isLoading ? "loading" : ""}`}>
       <div className="group-title">
         <p className="navbar-text">Planning événementiel</p>
       </div>
-      <div className="navbar-right">
+      <div className="navbar-left">
         <div className="navbar-text">
-          <FontAwesomeIcon icon={faUser} style={{ marginRight: "5px" }} />
+          <FontAwesomeIcon icon={faUsers} style={{ marginRight: "5px" }} />
           <div className="group-title">
-            <a href="#">Bonjour User</a>
+            <a className="text-navBar" href="#">Les groupes</a>
+          </div>
+          <div className="navbar-text">
+            <FontAwesomeIcon
+              icon={faCalendarDay}
+              style={{ marginRight: "5px" }}
+            />
+            <div className="group-title">
+              <a className="text-navBar" href="#">
+                Créer un événement
+              </a>
+            </div>
+          </div>
+          <div className="navbar-text">
+            <FontAwesomeIcon icon={faUser} style={{ marginRight: "5px" }} />
+            <div className="group-title">
+              <a className="text-navBar" href="#">Bonjour User</a>
+            </div>
           </div>
         </div>
       </div>
