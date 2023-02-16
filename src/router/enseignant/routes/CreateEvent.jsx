299e54d5ad508be904ede193087e1e5cfbf9173d
Hurleaa <114773436+Yamnyr@users.noreply.github.com>
2023-02-16 10:59:39 +0100

    route

---
+++ src/router/enseignant/routes/CreateEvent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventAdd from "../../../components/EventAdd";
+
+function CreateEvent() {
+    return (
+        <div>
+            <EventAdd />
+        </div>
+    );
+}
+
+export default CreateEvent;
