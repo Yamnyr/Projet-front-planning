947e3cd0cd40002dbad0713871c1c9e110875688
XyloxExe <maximec02@live.fr>
2023-02-16 16:52:41 +0100

    Mise a jours

--- src/components/Groupe.jsx
+++ src/components/Groupe.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { fetchDeleteGroupe, fetchAllGroupes } from "../services/api/groupeApi";
 
 function Groupe() {
@@ -143,6 +144,11 @@ function Groupe() {
               </>
             )}
             <button onClick={handleModalClose}>Fermer</button>
+            <Link to={`/edit/groupe/${modalGroup.id}`} className="text-navBar">
+              <button style={{ marginLeft: "20px" }} onClick="#">
+                Modifier
+              </button>
+            </Link>
             <button style={{ marginLeft: "20px" }} onClick="#">
               Modifier
             </button>
