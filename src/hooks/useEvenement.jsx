ae0a6a8c0280ab7ab141d3520108818a30bb00c3
Salmi Mustapha <mustapha.salmi08000@gmail.com>
2023-02-24 14:18:53 +0100

    correction edit evenement

--- src/hooks/useEvenement.jsx
+++ src/hooks/useEvenement.jsx
@@ -1,61 +1,60 @@
 import React, { useEffect, useState } from "react";
-import {
-    fetchAllGroupes,
-} from "../services/api/groupeApi";
-import UserContext from "../Context/index.js";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import {fetchAllEvenements, fetchDeleteEvenement} from "../services/api/evenementApi.js";
+import { fetchAllGroupes } from "../services/api/groupeApi";
+import UserContext from "../Context/index.js";
+import {
+  fetchAllEvenements,
+  fetchDeleteEvenement,
+} from "../services/api/evenementApi.js";
 
 export default function useEvenement() {
+  const [evenementList, setEvenementList] = useState([]);
+  const [search, setSearch] = useState("");
+  4;
 
-    const [evenementList, setEvenementList] = useState([]);
-    const [search, setSearch] = useState("");4
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalEvent, setModalEvent] = useState({});
 
+  const handleEventClick = (event) => {
+    setModalOpen(true);
+    setModalEvent(event);
+  };
 
-    const [modalOpen, setModalOpen] = useState(false);
-    const [modalEvent, setModalEvent] = useState({});
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
 
-    const handleEventClick = (event) => {
-        setModalOpen(true);
-        setModalEvent(event);
-    };
+  useEffect(() => {
+    fetchAllEvenements().then((response) => {
+      setEvenementList(response["hydra:member"]);
+    });
+  }, [evenementList]);
+  const deleteEvent = (id) => {
+    fetchDeleteEvenement(id).then(handleModalClose);
+  };
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    fetchAllEvenements({ e: search }).then((response) => {
+      setEvenementList(response["hydra:member"]);
+    });
+  };
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
-    const handleModalClose = () => {
-        setModalOpen(false);
-    };
-
-    useEffect(() => {
-        fetchAllEvenements().then((response) => {
-            setEvenementList(response["hydra:member"]);
-        });
-    }, [evenementList]);
-    const deleteEvent = (id) => {
-        fetchDeleteEvenement(id).then(handleModalClose);
-    }
-    const handleSearchSubmit = (event) => {
-        event.preventDefault();
-        fetchAllEvenements({ e: search }).then((response) => {
-            setEvenementList(response["hydra:member"]);
-        });
-    };
-    const handleSearchChange = (event) => {
-        setSearch(event.target.value);
-    };
-
-    const filteredEvenements = evenementList.filter((evenement) =>
-        evenement.lib_evenement.toLowerCase().includes(search.toLowerCase())
-    );
-    return {
-        handleSearchSubmit,
-        handleSearchChange,
-        handleEventClick,
-        handleModalClose,
-        deleteEvent,
-        filteredEvenements,
-        modalOpen,
-        modalEvent,
-        search,
-    };
-
+  const filteredEvenements = evenementList.filter((evenement) =>
+    evenement.lib_evenement.toLowerCase().includes(search.toLowerCase())
+  );
+  return {
+    handleSearchSubmit,
+    handleSearchChange,
+    handleEventClick,
+    handleModalClose,
+    deleteEvent,
+    filteredEvenements,
+    modalOpen,
+    modalEvent,
+    search,
+  };
 }
--- src/router/enseignant/routes/EditEvenement.jsx
+++ src/router/enseignant/routes/EditEvenement.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import EditEvenement from "../../../components/EditEvenement";
 
 function EditEvent() {
-    return (
-        <div>
-            <EditEvenement />
-        </div>
-    );
+  return (
+    <div>
+      <EditEvenement />
+    </div>
+  );
 }
 
 export default EditEvent;
